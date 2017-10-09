69231b8e6fa67107776d8c5d8fd95be1b2d7742e
Jeffrey Ren <jeffyiren@gmail.com>
2017-10-09 15:43:02 -0700

    Create skeleton for board.rb and piece.rb

---
+++ chess/board.rb
@@ -0,0 +1,43 @@
+require "piece.rb"
+
+class Board
+
+  attr_accessor :grid
+
+  def initialize
+    @grid = make_grid
+  end
+
+  def [](pos)
+
+  end
+
+  def []=(pos, piece)
+
+  end
+
+  def dup()
+
+  end
+
+  def move_piece(color, from_pos, to_pos)
+
+  end
+
+  def move_piece!(from_pos, to_pos)
+
+  end
+
+  def checkmate?()
+
+  end
+
+  protected
+  def make_starting_grid
+    Array.new(8) { Array.new(8) }
+  end
+
+  def find_king(color)
+
+  end
+end
---
+++ chess/piece.rb
@@ -0,0 +1,28 @@
+class Piece
+
+
+
+  def initialize
+  end
+
+  def to_s()
+
+  end
+
+  def empty?()
+
+  end
+
+  def symbol()
+
+  end
+
+  def valid_moves()
+
+  end
+
+  private
+  def move_into_check(to_pos)
+
+  end
+end
