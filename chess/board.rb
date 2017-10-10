2df26b24d2d8ef08a7bfc09221e30bdc09c20019
Jeffrey Ren <jeffyiren@gmail.com>
2017-10-09 17:58:44 -0700

    Initialize board and pieces

--- chess/board.rb
+++ chess/board.rb
@@ -1,43 +1,48 @@
-require "piece.rb"
+require_relative 'piece.rb'
+require 'byebug'
 
 class Board
 
   attr_accessor :grid
 
   def initialize
-    @grid = make_starting_grid
+    make_starting_grid
   end
 
   def [](pos)
-
+    x, y = pos
+    grid[x][y]
   end
 
   def []=(pos, piece)
-
+    x, y = pos
+    grid[x][y] = piece
   end
 
-  def dup()
-
+  def move_piece(from_pos, to_pos)
+    # will eventually need to check color of piece
+    # grid at from_pos becomes null, to_pos becomes piece
+    piece = grid[from_pos]
+    # if piece.empty?
+    #   raise BoardError
+    # end
+    self[from_pos] = nil
+    self[to_pos] = piece
+    piece.pos = [to_pos]
   end
 
-  def move_piece(color, from_pos, to_pos)
+  def add_piece(pos, symbol)
 
+    piece = Piece.new(pos, symbol)
+    # debugger
+    self[pos] = piece
   end
 
-  def move_piece!(from_pos, to_pos)
 
-  end
 
-  def checkmate?()
 
-  end
-
-  protected
   def make_starting_grid
-    Array.new(8) { Array.new(8) }
+    @grid = Array.new(8) { Array.new(8) }
   end
 
-  def find_king(color)
-
-  end
 end
--- chess/piece.rb
+++ chess/piece.rb
@@ -1,8 +1,7 @@
-require 'modules.rb'
+require_relative 'modules.rb'
 
 class Piece
-
-
+  attr_accessor :pos
 
   def initialize(pos, symbol = nil)
     @pos = pos
@@ -14,7 +13,7 @@ class Piece
   end
 
   def empty?()
-
+    @symbol.nil?
   end
 
   def symbol()
@@ -31,135 +30,137 @@ class Piece
   end
 end
 
-class King < Piece
-  include Stepable
-
-  def initialize(pos)
-    @pos = pos
-    @symbol = :K
-  end
-
-  def symbol()
-
-  end
-
-  protected
-  def  move_diffs()
-
-  end
-
-end
-
-class Knight < Piece
-
-  include Stepable
-  def initialize(pos)
-    @pos = pos
-    @symbol = :N
-  end
-
-  def symbol()
-
-  end
-
-  protected
-  def  move_diffs()
-
-  end
-
-end
-
-class Pawn < Piece
-
-  def initialize(pos)
-    @pos = pos
-    @symbol = :P
-  end
-
-  def symbol()
-
-  end
-
-  def moves()
-
-  end
-
-  protected
-  def at_start_row?()
-
-  end
-
-  def forward_dir()
-
-  end
-
-  def forward_steps()
-
-  end
-
-  def side_attacks()
-
-  end
-end
-
-class Bishop < Piece
-  include Slideable
-  def initialize(pos)
-    @pos = pos
-    @symbol = :B
-  end
-
-  def symbol()
-
-  end
-
-  protected
-  def  move_dirs()
-
-  end
-end
-
-class Rook < Piece
-  include Slideable
-  def initialize(pos)
-    @pos = pos
-    @symbol = :R
-  end
-
-  def symbol()
-
-  end
-
-  protected
-  def  move_dirs()
-
-  end
-end
-
-class Queen < Piece
-  include Slideable
-  def initialize(pos)
-    @pos = pos
-    @symbol = :Q
-  end
-
-  def symbol()
-
-  end
-
-  protected
-  def  move_dirs()
-
-  end
-end
-
-class NullPiece < Piece
-  def initialize
-    super
-  end
-
-  def moves()
-
-  end
-
-end
+# class King < Piece
+#   include Stepable
+#
+#   def initialize(pos)
+#     @pos = pos
+#     @symbol = :K
+#   end
+#
+#   def symbol()
+#
+#   end
+#
+#   protected
+#   def  move_diffs()
+#
+#   end
+#
+# end
+#
+# class Knight < Piece
+#
+#   include Stepable
+#   def initialize(pos)
+#     @pos = pos
+#     @symbol = :N
+#   end
+#
+#   def symbol()
+#
+#   end
+#
+#   protected
+#   def  move_diffs()
+#
+#   end
+#
+# end
+#
+# class Pawn < Piece
+#
+#   def initialize(pos)
+#     @pos = pos
+#     @symbol = :P
+#   end
+#
+#   def symbol()
+#
+#   end
+#
+#   def moves()
+#
+#   end
+#
+#   protected
+#   def at_start_row?()
+#
+#   end
+#
+#   def forward_dir()
+#
+#   end
+#
+#   def forward_steps()
+#
+#   end
+#
+#   def side_attacks()
+#
+#   end
+# end
+#
+# class Bishop < Piece
+#   include Slideable
+#   def initialize(pos)
+#     @pos = pos
+#     @symbol = :B
+#   end
+#
+#   def symbol()
+#
+#   end
+#
+#   protected
+#   def  move_dirs()
+#
+#   end
+# end
+#
+# class Rook < Piece
+#   include Slideable
+#   def initialize(pos)
+#     @pos = pos
+#     @symbol = :R
+#   end
+#
+#   def symbol()
+#
+#   end
+#
+#   protected
+#   def  move_dirs()
+#
+#   end
+# end
+#
+# class Queen < Piece
+#   include Slideable
+#   def initialize(pos)
+#     @pos = pos
+#     @symbol = :Q
+#   end
+#
+#   def symbol()
+#
+#   end
+#
+#   protected
+#   def  move_dirs()
+#
+#   end
+# end
+#
+# class NullPiece < Piece
+#   include Singleton
+#
+#   def initialize
+#     super
+#   end
+#
+#   def moves()
+#
+#   end
+#
+# end
